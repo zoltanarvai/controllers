03e0e03e9e4ea8b4fda607f3f2701147a8f71efe
Cal Leung <cleun007@gmail.com>
2022-04-06 15:55:54 -0700

    Fix non-BN call in AssetsContractController

--- src/assets/AssetsContractController.ts
+++ src/assets/AssetsContractController.ts
@@ -386,7 +386,7 @@ export class AssetsContractController extends BaseController<
             tokensToDetect.forEach((tokenAddress, index) => {
               const balance: BN = result[index];
               /* istanbul ignore else */
-              if (!balance.isZero()) {
+              if (String(balance) !== '0') {
                 nonZeroBalances[tokenAddress] = balance;
               }
             });
